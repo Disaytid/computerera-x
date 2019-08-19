b2304f1434edb27fb27bf27543fda21e6a790caa
Skvipers <skvipers@gmail.com>
2019-08-19 14:55:37 +0300

    Small fix

---
+++ Computer Era X/Models/RealEstateAgencyModel.cs
@@ -0,0 +1,27 @@
+﻿using Computer_Era_X.DataTypes.Objects;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Computer_Era_X.Models
+{
+    [NotMapped]
+    public class HousingSale : House
+    {
+        public BaseCurrencies Currency { get; set; }
+        public HousingSale(House house)
+        {
+            Id = house.Id;
+            Name = house.Name;
+            Area = house.Area;
+            StorageSize = house.StorageSize;
+            Rent = house.Rent;
+            Price = house.Price;
+            CommunalPayments = house.CommunalPayments;
+            Location = house.Location;
+            Distance = house.Distance;
+            IsPurchase = house.IsPurchase;
+            IsRent = house.IsRent;
+            IsCreditPurchase = house.IsCreditPurchase;
+            Image = house.Image;
+        }
+    }
+}
--- Computer Era X/ViewModels/Forms/Buildings/RealEstateAgencyVM.cs
+++ Computer Era X/ViewModels/Forms/Buildings/RealEstateAgencyVM.cs
@@ -1,10 +1,13 @@
 ﻿using Computer_Era_X.DataTypes.Objects;
+using Computer_Era_X.Models;
 using System.Collections.ObjectModel;
 
 namespace Computer_Era_X.ViewModels
 {
     partial class GameVM
     {
+        private Collection<HousingSale> housingSale = new Collection<HousingSale>();
+
         partial void RealEstateAgencyInit()
         {
             
